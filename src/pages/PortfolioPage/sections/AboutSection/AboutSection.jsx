6465304e6bba141aaf95e79dbba0822f408edb80
CallumB04 <callumburgoyne04@gmail.com>
2025-03-01 18:36:09 +0200

    feat: update about sections

--- src/pages/PortfolioPage/sections/AboutSection/AboutSection.jsx
+++ src/pages/PortfolioPage/sections/AboutSection/AboutSection.jsx
@@ -1,30 +1,30 @@
-import { useRef } from 'react';
-import './AboutSection.css';
-import { useEffect } from 'react';
+import { useRef } from "react";
+import "./AboutSection.css";
+import { useEffect } from "react";
 
 const AboutSection = ({ sectionRef }) => {
-
     // storing skills in 2D arrays, <skill, hover text, devicon class>
     // automatically maps to JSX in respective sections
     // for non devicon icons, arrays will have length of 2 <skill/hover text, img url>
     const skillsLanguages = [
-        ["python", "python", "python"], 
-        ["html", "html", "html5"], 
+        ["python", "python", "python"],
+        ["html", "html", "html5"],
         ["css", "css", "css3"],
-        ["cpp", "c\+\+", "cplusplus"],
+        ["cpp", "c++", "cplusplus"],
         ["js", "javascript", "javascript"],
-        ["ts", "typescript", "typescript"]];
+        ["ts", "typescript", "typescript"],
+    ];
     const skillsFworksAndLibs = [
         ["react", "react", "react"],
         ["tailwind", "tailwind css", "tailwindcss"],
-        ["sfml", "SFML.svg"]
+        ["sfml", "SFML.svg"],
     ];
     const skillsOther = [
         ["git", "git", "git"],
         ["github", "github", "github"],
         ["linux", "linux", "linux"],
         ["docker", "docker", "docker"],
-        ["sass", "sass", "sass"]
+        ["sass", "sass", "sass"],
     ];
 
     // references to download cv button elements, for animation
@@ -43,7 +43,7 @@ const AboutSection = ({ sectionRef }) => {
             CVButtonIcon.style.color = "var(--highlight-color)";
             CVButtonIcon.style.left = "50%";
             CVButtonText.style.display = "none";
-            CVButtonText.style.animation = "400ms fade-cv-btn-text linear";         
+            CVButtonText.style.animation = "400ms fade-cv-btn-text linear";
         });
 
         CVButton.addEventListener("mouseout", () => {
@@ -52,106 +52,155 @@ const AboutSection = ({ sectionRef }) => {
             CVButtonIcon.style.left = "calc(18% - 4px)";
             CVButtonText.style.display = "block";
         });
-    }, [])
-    
+    }, []);
+
     return (
-        <section className='portfolio-section dark-mode' id='about-section' ref={sectionRef}>
-            <h2 className='section-title about-section-title'>About me</h2>
-            <p className='section-description' id='about-section-description'>
-                Hi, I'm Callum, a <strong>20 year old</strong> self-taught Software Developer 
-                from <strong>Kent, United Kingdom</strong>. I'm passionate about creating 
-                efficient software, with experience in <strong>Web Development</strong>
-                , <strong>Game Development</strong>, and <strong>Backend Systems</strong>. 
-                I'm currently seeking a <strong>Level 4 Apprenticeship</strong> in any 
-                area of Software Development, and eager to bring my skills and passion 
-                for learning to a collaborative work environment!
+        <section
+            className="portfolio-section dark-mode"
+            id="about-section"
+            ref={sectionRef}
+        >
+            <h2 className="section-title about-section-title">About me</h2>
+            <p className="section-description" id="about-section-description">
+                Hi, I'm Callum, a <strong>20 year old</strong> self-taught
+                Software Developer from <strong>Kent, United Kingdom</strong>.
+                I'm passionate about creating efficient software, with
+                experience in <strong>Full-Stack Web Development</strong>,{" "}
+                <strong>Game Development</strong>, and more. I'm currently
+                pursuing a <strong>Level 4 Apprenticeship</strong> in Software
+                Development, and eager to continue developing my knowledge and
+                skills within the industry!
             </p>
 
-            <a ref={CVButtonRef} id='download-cv-button' href='/assets/Callum_Burgoyne_CV.pdf' download>
-                <i ref={CVButtonIconRef} className='fas fa-download'></i> 
+            <a
+                ref={CVButtonRef}
+                id="download-cv-button"
+                href="/assets/Callum_Burgoyne_CV.pdf"
+                download
+            >
+                <i ref={CVButtonIconRef} className="fas fa-download"></i>
                 <p ref={CVButtonTextRef}>Download CV</p>
             </a>
 
-            <h2 className='section-title about-section-title'>Tech Stack</h2>
+            <h2 className="section-title about-section-title">Tech Stack</h2>
 
-            <h3 className='section-title about-section-subtitle'>Languages</h3>
-            <div className='about-section-skills-wrapper'>
+            <h3 className="section-title about-section-subtitle">Languages</h3>
+            <div className="about-section-skills-wrapper">
                 {skillsLanguages.map((skill) => {
                     return skill.length === 3 ? (
-                        <div key={skill[0]} className='about-section-skill' id={`about-skill-${skill[0]}`}>
+                        <div
+                            key={skill[0]}
+                            className="about-section-skill"
+                            id={`about-skill-${skill[0]}`}
+                        >
                             <i className={`devicon-${skill[2]}-plain`}></i>
-                            <style>{`
+                            <style>
+                                {`
                                 #about-skill-${skill[0]}::after {
                                     content: "${skill[1]}";
                                 }`}
                             </style>
                         </div>
-                    )
-                    : (
-                        <div key={skill[0]} className='about-section-skill non-devicon-skill' id={`about-skill-${skill[0]}`}>
-                            <img src={`../../../../assets/skill-icons/${skill[1]}`} className='non-devicon-icon' />
-                            <style>{`
+                    ) : (
+                        <div
+                            key={skill[0]}
+                            className="about-section-skill non-devicon-skill"
+                            id={`about-skill-${skill[0]}`}
+                        >
+                            <img
+                                src={`../../../../assets/skill-icons/${skill[1]}`}
+                                className="non-devicon-icon"
+                            />
+                            <style>
+                                {`
                                 #about-skill-${skill[0]}::after {
                                     content: "${skill[0]}";
                                 }`}
                             </style>
                         </div>
-                    )
-                } )}
+                    );
+                })}
             </div>
 
-            <h3 className='section-title about-section-subtitle'>Frameworks + Libraries</h3>
-            <div className='about-section-skills-wrapper'>
+            <h3 className="section-title about-section-subtitle">
+                Frameworks + Libraries
+            </h3>
+            <div className="about-section-skills-wrapper">
                 {skillsFworksAndLibs.map((skill) => {
                     return skill.length === 3 ? (
-                        <div key={skill[0]} className='about-section-skill' id={`about-skill-${skill[0]}`}>
+                        <div
+                            key={skill[0]}
+                            className="about-section-skill"
+                            id={`about-skill-${skill[0]}`}
+                        >
                             <i className={`devicon-${skill[2]}-plain`}></i>
-                            <style>{`
+                            <style>
+                                {`
                                 #about-skill-${skill[0]}::after {
                                     content: "${skill[1]}";
                                 }`}
                             </style>
                         </div>
-                    )
-                    : (
-                        <div key={skill[0]} className='about-section-skill non-devicon-skill' id={`about-skill-${skill[0]}`}>
-                            <img src={`/assets/skill-icons/${skill[1]}`} className='non-devicon-icon' />
-                            <style>{`
+                    ) : (
+                        <div
+                            key={skill[0]}
+                            className="about-section-skill non-devicon-skill"
+                            id={`about-skill-${skill[0]}`}
+                        >
+                            <img
+                                src={`/assets/skill-icons/${skill[1]}`}
+                                className="non-devicon-icon"
+                            />
+                            <style>
+                                {`
                                 #about-skill-${skill[0]}::after {
                                     content: "${skill[0]}";
                                 }`}
                             </style>
                         </div>
-                    )
-                } )}
+                    );
+                })}
             </div>
 
-            <h3 className='section-title about-section-subtitle'>Other Technologies</h3>
-            <div className='about-section-skills-wrapper'>
+            <h3 className="section-title about-section-subtitle">
+                Other Technologies
+            </h3>
+            <div className="about-section-skills-wrapper">
                 {skillsOther.map((skill) => {
                     return skill.length === 3 ? (
-                        <div key={skill[0]} className='about-section-skill' id={`about-skill-${skill[0]}`}>
+                        <div
+                            key={skill[0]}
+                            className="about-section-skill"
+                            id={`about-skill-${skill[0]}`}
+                        >
                             <i className={`devicon-${skill[2]}-plain`}></i>
-                            <style>{`
+                            <style>
+                                {`
                                 #about-skill-${skill[0]}::after {
                                     content: "${skill[1]}";
                                 }`}
                             </style>
                         </div>
-                    )
-                    : (
-                        <div key={skill[0]} className='about-section-skill non-devicon-skill' id={`about-skill-${skill[0]}`}>
-                            <img src={`../../../../assets/skill-icons/${skill[1]}`} className='non-devicon-icon' />
-                            <style>{`
+                    ) : (
+                        <div
+                            key={skill[0]}
+                            className="about-section-skill non-devicon-skill"
+                            id={`about-skill-${skill[0]}`}
+                        >
+                            <img
+                                src={`../../../../assets/skill-icons/${skill[1]}`}
+                                className="non-devicon-icon"
+                            />
+                            <style>
+                                {`
                                 #about-skill-${skill[0]}::after {
                                     content: "${skill[0]}";
                                 }`}
                             </style>
                         </div>
-                    )
-                } )}
+                    );
+                })}
             </div>
-
         </section>
     );
 };
--- src/pages/PortfolioPage/sections/HomeSection/HomeSection.jsx
+++ src/pages/PortfolioPage/sections/HomeSection/HomeSection.jsx
@@ -1,51 +1,72 @@
-import './HomeSection.css';
+import "./HomeSection.css";
 
 const HomeSection = ({ sectionRefs }) => {
-
     // function to scroll to other sections on button press
     const scrollToSection = (section) => {
-        section.current.scrollIntoView({ behavior: "smooth" })
-    }
+        section.current.scrollIntoView({ behavior: "smooth" });
+    };
 
     // button functions
-    const scrollToAbout = () => { scrollToSection(sectionRefs.aboutSection) ;}
-    const scrollToProjects = () => { scrollToSection(sectionRefs.projectsSection) ;}
-    
-    return (
-        <section className='portfolio-section light-mode' id='home-section' ref={sectionRefs.homeSection}>
-            <div id='home-section-content'>
-                <h1 className='section-title' id='home-section-title'>Hi, I'm Callum Burgoyne</h1>
+    const scrollToAbout = () => {
+        scrollToSection(sectionRefs.aboutSection);
+    };
+    const scrollToProjects = () => {
+        scrollToSection(sectionRefs.projectsSection);
+    };
 
-                <p className='section-description' id='home-section-description'>
-                    A self-taught <strong>Software Developer</strong>, looking for an Apprenticeship to start
-                    my Career within the Industry!
+    return (
+        <section
+            className="portfolio-section light-mode"
+            id="home-section"
+            ref={sectionRefs.homeSection}
+        >
+            <div id="home-section-content">
+                <h1 className="section-title" id="home-section-title">
+                    Hi, I'm Callum Burgoyne
+                </h1>
+
+                <p
+                    className="section-description"
+                    id="home-section-description"
+                >
+                    A self-taught <strong>Software Developer</strong>, currently
+                    pursuing a Level 4 Apprenticeship!
                 </p>
 
-                <div id='home-section-socials-wrapper'>
-                    <a href='https://github.com/CallumB04' target='_blank'>
-                        <i className='home-section-socials fab fa-github'></i>
+                <div id="home-section-socials-wrapper">
+                    <a href="https://github.com/CallumB04" target="_blank">
+                        <i className="home-section-socials fab fa-github"></i>
                     </a>
-                    <a href='https://www.linkedin.com/in/callum-burgoyne-1b411a324' target='_blank'>
-                        <i className='home-section-socials fab fa-linkedin'></i>
+                    <a
+                        href="https://www.linkedin.com/in/callum-burgoyne-1b411a324"
+                        target="_blank"
+                    >
+                        <i className="home-section-socials fab fa-linkedin"></i>
                     </a>
-                    <a href='https://instagram.com/calbgyn' target='_blank'>
-                        <i className='home-section-socials fab fa-instagram'></i>
+                    <a href="https://instagram.com/calbgyn" target="_blank">
+                        <i className="home-section-socials fab fa-instagram"></i>
                     </a>
-                    <a href='mailto:burgoynecallum04@gmail.com' target='_blank'>
-                        <i className='home-section-socials fa fa-envelope'></i>
+                    <a href="mailto:burgoynecallum04@gmail.com" target="_blank">
+                        <i className="home-section-socials fa fa-envelope"></i>
                     </a>
                 </div>
 
-                <div id='home-section-buttons-wrapper'>
-                    <div className='home-section-button' onClick={scrollToAbout}>
+                <div id="home-section-buttons-wrapper">
+                    <div
+                        className="home-section-button"
+                        onClick={scrollToAbout}
+                    >
                         About Me
-                        <i className='fa fa-arrow-right'></i>
+                        <i className="fa fa-arrow-right"></i>
                     </div>
-                    <div className='home-section-button' onClick={scrollToProjects}>
+                    <div
+                        className="home-section-button"
+                        onClick={scrollToProjects}
+                    >
                         My Projects
-                        <i className='fa fa-arrow-right'></i>
+                        <i className="fa fa-arrow-right"></i>
                     </div>
-                </div>   
+                </div>
             </div>
         </section>
     );
