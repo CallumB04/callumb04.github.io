08926969b4f64aa3d049cb4a073999dc545ef5c0
CallumB04 <callumburgoyne04@gmail.com>
2025-08-04 00:54:30 +0300

    feat: update project page navbar

--- src/pages/ProjectPage/Navbar/Navbar.jsx
+++ src/pages/ProjectPage/Navbar/Navbar.jsx
@@ -1,13 +1,11 @@
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
     return (
-        <nav className='project-navbar'>
-            <Link to='/'>
-                <i className='fa fa-house-chimney'></i>
-                <p>Home</p>
+        <nav className="project-navbar">
+            <Link to="/">
+                <i className="fa fa-house"></i>
             </Link>
         </nav>
     );
