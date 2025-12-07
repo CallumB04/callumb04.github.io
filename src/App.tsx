de875e79b2ebe3359bd8dd8991dd58a448a4b45c
Cal <callumburgoyne04@gmail.com>
2025-12-07 23:00:30 +0000

    feat: add pages to react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import IndexPage from "./pages/Index/IndexPage";
+import ProjectPage from "./pages/Project/ProjectPage";
+import BlogsPage from "./pages/Blogs/BlogsPage";
+import BlogPostPage from "./pages/BlogPost/BlogPostPage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes></Routes>
+            <Routes>
+                <Route path="/" element={<IndexPage />} />
+                <Route path="/project/:slug" element={<ProjectPage />} />
+                <Route path="/blogs" element={<BlogsPage />} />
+                <Route path="/blogs/:slug" element={<BlogPostPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
         </BrowserRouter>
     );
 }
