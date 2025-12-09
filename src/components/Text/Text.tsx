4624d24337abf21631d883bf7a71c0baab541465
Cal <callumburgoyne04@gmail.com>
2025-12-09 01:39:21 +0000

    feat: update text styling for variants

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,23 +1,29 @@
-import type { ReactNode } from "react";
+import { useMemo, type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface TextProps {
     children: ReactNode;
-    variant: "primary" | "secondary";
+    variant: "primary" | "secondary" | "highlight" | "highlight-soft";
     className?: string;
 }
 
 const Text = ({ children, variant, className }: TextProps) => {
+    // update text color in tailwind classes when variant changes
+    const variantClassName = useMemo(() => {
+        switch (variant) {
+            case "primary":
+                return "text-text-primary";
+            case "secondary":
+                return "text-text-secondary";
+            case "highlight":
+                return "text-highlight";
+            case "highlight-soft":
+                return "text-highlight-soft";
+        }
+    }, [variant]);
+
     return (
-        <p
-            className={twMerge(
-                "font-primary",
-                variant === "primary"
-                    ? "text-text-primary"
-                    : "text-text-secondary",
-                className
-            )}
-        >
+        <p className={twMerge("font-primary", variantClassName, className)}>
             {children}
         </p>
     );
