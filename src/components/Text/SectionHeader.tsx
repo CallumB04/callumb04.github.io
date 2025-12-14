bd149e2a29c1392b1535a229af59ba00981064a3
Cal <callumburgoyne04@gmail.com>
2025-12-14 00:44:30 +0000

    feat: create section header component

---
+++ src/components/Text/SectionHeader.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+import Text from "./Text";
+
+interface SectionHeaderProps {
+    children: ReactNode;
+}
+
+const SectionHeader = ({ children }: SectionHeaderProps) => {
+    return (
+        <Text variant="secondary" className="text-sm uppercase">
+            {children}
+        </Text>
+    );
+};
+
+export default SectionHeader;
