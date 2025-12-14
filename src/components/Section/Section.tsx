ebe6b14de08bbabbd43ed8fd824db9793b4c3583
Cal <callumburgoyne04@gmail.com>
2025-12-14 02:21:33 +0000

    feat: add optional subheader to section heading

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,18 +1,27 @@
 import type { ReactNode } from "react";
 import SectionHeader from "../Text/SectionHeader";
+import Text from "../Text/Text";
 
 interface SectionProps {
     children?: ReactNode;
     header: string;
+    subheader?: string; // optional subheader, justify between with header
     id?: string;
 }
 
-const Section = ({ children, header, id }: SectionProps) => {
+const Section = ({ children, header, subheader, id }: SectionProps) => {
     return (
-        <div id={id} className="flex flex-col gap-6">
-            <SectionHeader>{header}</SectionHeader>
+        <section id={id} className="flex flex-col gap-6">
+            <span className="flex w-full items-center justify-between">
+                <SectionHeader>{header}</SectionHeader>
+                {subheader && (
+                    <Text variant="secondary" className="text-xs">
+                        {subheader}
+                    </Text>
+                )}
+            </span>
             {children}
-        </div>
+        </section>
     );
 };
 
