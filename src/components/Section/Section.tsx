862bfd2c96427f2cb4e700830a0255825f915676
Cal <callumburgoyne04@gmail.com>
2025-12-24 23:47:14 +0000

    feat: add optional redirect text option to sections

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,16 +1,25 @@
 import type { ReactNode } from "react";
 import SectionHeader from "../Text/SectionHeader";
+import RedirectText from "../Text/RedirectText";
 
 interface SectionProps {
     children?: ReactNode;
     header: string;
+    redirect?: { text: string; to: string };
     id?: string;
 }
 
-const Section = ({ children, header, id }: SectionProps) => {
+const Section = ({ children, header, redirect, id }: SectionProps) => {
     return (
         <section id={id} className="flex flex-col gap-6">
-            <SectionHeader>{header}</SectionHeader>
+            <span className="flex w-full items-center justify-between">
+                <SectionHeader>{header}</SectionHeader>
+                {redirect && (
+                    <RedirectText to={redirect.to}>
+                        {redirect.text}
+                    </RedirectText>
+                )}
+            </span>
             {children}
         </section>
     );
