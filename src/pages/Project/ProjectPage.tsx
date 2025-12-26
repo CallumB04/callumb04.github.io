edbedc8fa6949662b682672cd00f1244159835d3
Cal <callumburgoyne04@gmail.com>
2025-12-26 23:58:30 +0000

    feat: load project from slug in URL

--- src/pages/Project/ProjectPage.tsx
+++ src/pages/Project/ProjectPage.tsx
@@ -1,12 +1,38 @@
 import { useParams } from "react-router-dom";
+import Section from "../../components/Section/Section";
+import { useEffect, useState } from "react";
+import type { Project } from "../../data/models";
+import { loadProjectBySlug } from "../../data/loader";
+import usePageTitle from "../../hooks/usePageTitle";
 
 const ProjectPage = () => {
+    // get project slug from url to load data
     const { slug } = useParams();
 
-    // add page title when pulled project info
-    // usePageTitle("<project name> | Callum Burgoyne")
+    const [project, setProject] = useState<Project | undefined>(undefined);
 
-    return <main>ProjectPage</main>;
+    // load project data into state
+    useEffect(() => {
+        const loadProjectData = async () => {
+            if (slug) {
+                const resp = await loadProjectBySlug(slug);
+                if (resp) {
+                    setProject(resp);
+                }
+            }
+        };
+
+        loadProjectData();
+    }, [slug]);
+
+    usePageTitle(project?.title + " | Callum Burgoyne");
+
+    return (
+        <main>
+            <Section header="Project Info"></Section>
+            <Section header="Project Images"></Section>
+        </main>
+    );
 };
 
 export default ProjectPage;
